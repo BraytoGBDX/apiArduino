67974ed046a3b0e9490415215ffa29c875f7debb
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2024-03-08 17:37:25 -0600

    Add: Creando el modelo de los sensores

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const C = console.log.bind(console.log)
 import app from "./app.js";
 import conectar from "./database.js"
+import chalk from 'chalk'
+
+const success = chalk.bold.underline.blue;
 
 
 app.listen(process.env.SERVER_PORT,()=>{
-    C("Server on port: " + process.env.SERVER_PORT)
+    C(success("Server on port: " + process.env.SERVER_PORT))
 })
